1e7f579dcd5ac54c16cf5e7488af4ab5edbcfe6a
Cintamani <cintamani_whyk@naver.com>
2018-10-18 01:47:44 +0900

    컴포넌트 최적화

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import './TodoItem.css';
 
 class TodoItem extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.checked !== nextProps.checked;
+    }
+
     render() {
         const {text, checked, id, onToggle, onRemove} = this.props;
         // text: todo의 내용, checked: 체크박스의 상태, id: todo의 고유아이디, onToggle: 체크박스를 키고 끄는 함수, onRemove: 아이템을 삭제시키는 함수
+
         return (
             <div className="todo-item" onClick={() => onToggle(id)}>
                 <div className="remove" onClick={(e) => {
